dce9d5bb82ff5e330f8b11860bc86e2a152a4eac
Tisha Murvihill <tmurv@shaw.ca>
2025-08-02 10:16:10 -0600

    pass full client from send-email

--- src/controllers/get-email.js
+++ src/controllers/get-email.js
@@ -1,11 +1,11 @@
 module.exports.getEmail = (options) => {
-  const {fullClient} = options;
+  const { fullClient } = options;
 
-  return ({
-      from: `TECH <tech@take2tech.ca>`,
-      to: fullClient.email, // can be list 'tmurv@fdjfl.com temsk@fjdks.com'
-      subject: "Contact Message Received",
-      html: `
+  return {
+    from: `TECH <tech@take2tech.ca>`,
+    to: fullClient.email, // can be list 'tmurv@fdjfl.com temsk@fjdks.com'
+    subject: "Contact Message Received",
+    html: `
             <!DOCTYPE html>
             <html lang="en">
                 <head>
@@ -16,12 +16,12 @@ module.exports.getEmail = (options) => {
             <body>
                 <h2>Hello ${fullClient.name}! </h2>
                 <p>We have received a message on your contact page. Here are the details: </p>
-                <p>Contact Name: ${options.customer.name}</p>
-                <p>Contact Email: ${options.customer.email}</p>
-                <p>Contact Phone: ${options.customer.phone}</p>
-                <p>Contact Message: ${options.customer.message}</p>
+                <p>Contact Name: ${options.customer?.name ?? options.name}</p>
+                <p>Contact Email: ${options.customer?.email ?? options.email}</p>
+                <p>Contact Phone: ${options.customer?.phone ?? options.phone}</p>
+                <p>Contact Message: ${options.customer?.message ?? options.message}</p>
             </body>
             </html>
-            `
-  })
+            `,
+  };
 };
